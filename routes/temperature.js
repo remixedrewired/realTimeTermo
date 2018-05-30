5020ddf2149af1b9181d76a370b49a3e1f3512cc
Anton Shapovalov <anton.shapovalov@chisw.com>
2018-05-30 04:53:23 +0300

    remane; changed pics; added multi instead of temp/light

--- routes/temperature.js
+++ routes/temperature.js
@@ -1,52 +1,45 @@
-const pubnub = require('pubnub')({
+const pubnub = require('pubnub')
+const PubNub = new pubnub({
   publish_key: 'pub-c-1a33e74f-10fe-45ad-b6b4-f2dda25251c6',
   subscribe_key: 'sub-c-ebe2b4f8-2506-11e8-bb29-5a43d096f02f'
 });
 const channel = 'temperature-ds18b20';
 const five = require('johnny-five');
+const board = new five.Board();
 
 let temp = 0;
-let light = 0;
+let humidity = 0;
 let tempinf = 0;
 
 function publish() {
   let data = {
     'temperature': temp,
-    'tempf': tempinf,
-    'light': light
+    'humidity': humidity
   };
-  pubnub.publish({
+  PubNub.publish({
     channel: channel,
     message: data,
   });
 }
-
-five.Board().on('ready', () => {
-  const temperature = new five.Thermometer({
-    controller: 'DS18B20',
-    pin: 12, 
+board.on('ready', () => {
+  const multi = new five.Multi({
+    controller: "DHT22_I2C_NANO_BACKPACK",
+    pin: 2,
     freq: 1000
   });
-  const photoresistor = new five.Sensor({
-    pin: 'A1',
-    freq: 1000
-  });
-  let temptoscreen = '';
 
-  temperature.on('data', (e) => {
-    console.log(e.celsius + '°C', e.fahrenheit + '°F');
-    temp = e.celsius;
-    tempinf = e.fahrenheit;
-    temptoscreen = temp ;
-  });
+  multi.on('data', (e) => {
+    temp = e.thermometer.celsius;
+    humidity = e.hygrometer.relativeHumidity;
+    console.log("Thermometer");
+    console.log("  celsius           : ", e.thermometer.celsius);
+    console.log("  fahrenheit        : ", e.thermometer.fahrenheit);
+    console.log("  kelvin            : ", e.thermometer.kelvin);
+    console.log("--------------------------------------");
 
-  five.Board().repl.inject({
-    pot: photoresistor
-  });
-
-  photoresistor.on('data', (e) => {
-    console.log('photoresistor: ' + e);
-    light = e / 15;
+    console.log("Hygrometer");
+    console.log("  relative humidity : ", e.hygrometer.relativeHumidity);
+    console.log("--------------------------------------");
   });
 
   setInterval(publish, 1000);
